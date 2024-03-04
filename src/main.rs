5c7f5e5b6da94bc184d0f2da779a791031496068
Mark Nash <abacefabmicroblast@hotmail.com>
2024-03-03 23:09:02 -0800

    listens for metric data from clients

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-#[macro_use] extern crate rocket;
-use serde::{Deserialize, Serialize};
-use rocket::serde::{json::Json};
+#[macro_use]
+extern crate rocket;
+use rocket::serde::json::Json;
+use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
-struct Tang<'r> {
-    description: &'r str,
-    complete: bool
+struct Tang {
+    cpu_used: f64,
 }
 
-#[post("/metric_data", data = "<task>")]
-fn new_data(task: Json<Tang<'_>>) {
+#[post("/cpu_usage", data = "<task>")]
+fn new_data(task: Json<Tang>) {
     println!("{:?}", task);
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![new_data])
+    rocket::build().mount("/metric_data", routes![new_data])
 }
